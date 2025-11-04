31b0c3472065d958edf011bec3a2a02d36e6bcf6
sunchengzhu <sunchengzhu@cryptape.com>
2025-11-04 20:22:42 +0800

    add ckb init

--- sync.sh
+++ sync.sh
@@ -58,6 +58,10 @@ fi
 ./"mainnet_ckb_${ckb_version}_x86_64-unknown-linux-gnu"/ckb --version >"$result_log"
 
 # 修改mainnet ckb.toml
+sudo ./ckb init --chain mainnet --force
+echo "------------------------------------------------------------"
+grep 'spec =' ckb.toml
+
 cd "mainnet_ckb_${ckb_version}_x86_64-unknown-linux-gnu" || exit
 grep "^listen_address =" ckb.toml
 new_listen_address="0.0.0.0:8114"
@@ -78,6 +82,10 @@ tail -n 8 ckb.toml
 cd ..
 
 # 修改testnet ckb.toml
+sudo ./ckb init --chain testnet --force
+echo "------------------------------------------------------------"
+grep 'spec =' ckb.toml
+
 cd "testnet_ckb_${ckb_version}_x86_64-unknown-linux-gnu" || exit
 grep "^listen_address =" ckb.toml
 new_listen_address="0.0.0.0:8115"
